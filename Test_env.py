fcb897928362e54a2a8fb1dc873e0fd27734778e
AQAtesting <strayers@mail.ru>
2022-09-18 16:19:31 +0400

    Initial commit

---
+++ Test_env.py
@@ -0,0 +1,8 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = input("Сумма вклада:")
+deposit_per_cent = list(per_cent.values())
+deposit = [num * int(money)/100 for num in deposit_per_cent]
+deposit = [int(x) for x in deposit]
+max_deposit = max(deposit)
+print("Накопленные средства на вкладе за год разных банках:", deposit)
+print("Максимальная сумма, которую вы можете заработать —", max_deposit)
