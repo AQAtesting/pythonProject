ae7d7a756e6b1aba12e25f3a8fe77dfb9d3cb72e
AQAtesting <strayers@mail.ru>
2022-09-18 16:37:18 +0400

    2

--- Test_env.py
+++ Test_env.py
@@ -4,5 +4,5 @@ deposit_per_cent = list(per_cent.values())
 deposit = [num * int(money)/100 for num in deposit_per_cent]
 deposit = [int(x) for x in deposit]
 max_deposit = max(deposit)
-print("Накопленные средства на вкладе за год разных банках:", deposit)
+print("Накопленные средства на вкладе за год в разных банках:", deposit)
 print("Максимальная сумма, которую вы можете заработать —", max_deposit)
